39dd238a19c01f6d66e410716199c77839b48889
TitanDz <dev@wakkadoodle.co>
2025-10-31 17:53:39 -0600

    Update theme.dart

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const Color primary = Color(0xFF6366F1);
-  static const Color primaryLight = Color(0xFFEEF2FF);
-  static const Color primaryDark = Color(0xFF4338CA);
+  static const Color primary = Color(0xFF1273D4);
+  static const Color primaryLight = Color(0xFFE3F2FD);
+  static const Color primaryDark = Color(0xFF0D5BA8);
 
   static const Color secondary = Color(0xFF10B981);
   static const Color error = Color(0xFFEF4444);
